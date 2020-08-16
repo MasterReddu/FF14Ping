62a93b4e7462e6ef19ef6954f27011982b42f587
Reddy <gamesharkakaflash@hotmail.com>
2020-08-16 10:48:59 +1000

    Add project files.

---
+++ FFPing.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace FF14Ping
+{
+    partial class FFPing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FFPing));
+            this.myTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblLatency = new System.Windows.Forms.Label();
+            this.lblExit = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblLatency
+            // 
+            this.lblLatency.AutoSize = true;
+            this.lblLatency.BackColor = System.Drawing.Color.Transparent;
+            this.lblLatency.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLatency.Location = new System.Drawing.Point(10, 9);
+            this.lblLatency.Name = "lblLatency";
+            this.lblLatency.Size = new System.Drawing.Size(81, 24);
+            this.lblLatency.TabIndex = 0;
+            this.lblLatency.Text = "1000ms";
+            this.lblLatency.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FFPing_MouseDown);
+            this.lblLatency.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FFPing_MouseMove);
+            this.lblLatency.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FFPing_MouseUp);
+            // 
+            // lblExit
+            // 
+            this.lblExit.AutoSize = true;
+            this.lblExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExit.Location = new System.Drawing.Point(113, -1);
+            this.lblExit.Name = "lblExit";
+            this.lblExit.Size = new System.Drawing.Size(14, 16);
+            this.lblExit.TabIndex = 1;
+            this.lblExit.Text = "x";
+            this.lblExit.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FFPing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(129, 42);
+            this.Controls.Add(this.lblExit);
+            this.Controls.Add(this.lblLatency);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FFPing";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.FFPing_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FFPing_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FFPing_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FFPing_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer myTimer;
+        private System.Windows.Forms.Label lblLatency;
+        private System.Windows.Forms.Label lblExit;
+    }
+}
+
---
+++ FFPing.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+
+namespace FF14Ping
+{
+    public partial class FFPing : Form
+    {
+
+        private bool IsDrag = false;
+        private Point startPoint = new Point(0,0);
+        private void FFPing_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsDrag = true;
+            startPoint = new Point(e.X, e.Y);
+        }
+        private void FFPing_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsDrag = false;
+        }
+        private void FFPing_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsDrag)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this.startPoint.X, p.Y - this.startPoint.Y);
+            }
+        }
+
+
+
+        public FFPing()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void FFPing_Load(object sender, EventArgs e)
+        {
+            // To update the first time.
+            Ping();
+            //Tick-tock on the clock
+            //But the party don't stop...
+            var myTimer = new Timer { Interval = 1000 };
+            myTimer.Tick += (o, args) =>
+            {
+                Ping();
+            };
+            myTimer.Start();
+        }
+
+
+
+
+
+        private void Ping()
+        //Ping the Tonberry Server and toss it in the label
+        {
+            using (Ping p = new Ping())
+            {
+                lblLatency.Text = p.Send("124.150.157.24").RoundtripTime.ToString() + "ms";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
